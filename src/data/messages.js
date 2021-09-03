a56330b6bad1f6c39e5d328ffebbe16229dfb850
Иван Бибинин <bibivanya@gmail.com>
2021-09-03 19:12:28 +0400

    first commit

---
+++ src/data/messages.js
@@ -0,0 +1,4 @@
+const message1 = 'Hello, World!';
+const message2 = 'Hello, Vue!';
+
+export { message1, message2 };
---
+++ src/data/products.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: '/img/toothbrush.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: 21790,
+    image: '/img/phone.jpg',
+  },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 24690,
+    image: '/img/board.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    price: 14890,
+    image: '/img/phone-2.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: 8690,
+    image: '/img/phone-3.jpg',
+  },
+  {
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: 39990,
+    image: '/img/bicycle.jpg',
+  },
+  {
+    title: 'Гироскутер Razor Hovertrax 2.0',
+    price: 34990,
+    image: '/img/wheels.jpg',
+  },
+  {
+    title: 'Детский трюковой самокат Razor Grom',
+    price: 4990,
+    image: '/img/scooter.jpg',
+  },
+  {
+    title: 'Роллерсёрф Razor RipStik Air Pro',
+    price: 6690,
+    image: '/img/ripstik.jpg',
+  },
+  {
+    title: 'Наушники Sony',
+    price: 30690,
+    image: '/img/headphones.jpg',
+  },
+  {
+    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    price: 16690,
+    image: '/img/airpods.jpg',
+  },
+  {
+    title: 'Ноутбук Xiaomi Mi Gaming Laptop',
+    price: 77690,
+    image: '/img/XiaomiMiGamingLaptop.jpg',
+  },
+  {
+    title: 'Смартфон Poco F3 NFC 8/256Gb Черный Global Version',
+    price: 31990,
+    image: '/img/pocoF3.jpg',
+  },
+  {
+    title: 'Беспроводные наушники JBL T450BT',
+    price: 2990,
+    image: '/img/JBL-T450BT.jpg',
+  },
+  {
+    title: 'GIGABYTE B450M GAMING',
+    price: 5790,
+    image: '/img/GIGABYTE-B450M-GAMING.jpg',
+  },
+  {
+    title: 'Оперативная память HyperX HX430C15PB3/8 2x8 ГБ DDR4 (HX426C13PB3K2/16)',
+    price: 8130,
+    image: '/img/HyperX-HX430C15PB3_8.jpg',
+  },
+  {
+    title: 'Пылесос PHILIPS Performer FC9174/02, 2200Вт, красный',
+    price: 13690,
+    image: '/img/PHILIPS-Performer-FC917402.jpg',
+  },
+  {
+    title: 'Пылесос BOSCH BGLS482200, 2200Вт, черный',
+    price: 15790,
+    image: '/img/BOSCH-BGLS482200.jpg',
+  },
+  {
+    title: 'Жесткий диск WD GOLD WD2005FBYZ 2000ГБ 3,5" 7200RPM 128MB (SATA III) RE',
+    price: 10290,
+    image: '/img/WD-WD2005FBYZ.jpg',
+  },
+  {
+    title: 'Видеокамера Canon Legria HF G26',
+    price: 68130,
+    image: '/img/Canon_Legria_HFG26.jpg',
+  },
+  {
+    title: 'Экшн камера GoPro MAX',
+    price: 43690,
+    image: '/img/goProMax.jpg',
+  },
+  {
+    title: 'Карта памяти Samsung microSDXC Evo Plus Class 10 UHS-I U1',
+    price: 2190,
+    image: '/img/Samsung_micro_SDXC_UHS-I_U1.jpg',
+  },
+  {
+    title: 'Карта памяти Transcend 32GB UHS-I U1 microSD',
+    price: 490,
+    image: '/img/transcend_micro_sd.jpg',
+  },
+];
